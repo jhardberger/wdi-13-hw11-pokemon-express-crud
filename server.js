6edc0b6781da4d5461135765eb8e727eed75d7a5
John <john@Johns-MacBook-Pro.local>
2018-10-10 22:54:28 -0500

    commit 7: 7 restful routes

--- server.js
+++ server.js
@@ -1,21 +1,38 @@
-// server setup
+// server setup--------------------------------------//
+
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
+const bodyParser = require('body-parser');
+
+// database------------------------------------------//
+
+const Pokemon = require('./pokemon');
+
+// middleware----------------------------------------//
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
+//listener-------------------------------------------//
 
 app.listen(3000, ()=>{
   console.log("App is running on port 3000");
 });
 
-// database
-const Pokemon = require('./pokemon');
+//routes---------------------------------------------//
 
-/*-----------------------------------------------
-Unless I missed something major, I don't recall
-learning static? I took a look at it, but I'm 
-having trouble making sense of it, so I'm going
-to omit this for now...
------------------------------------------------*/
+// new route get
+app.get('/pokemon/new', (req, res) => {
+	res.render('new.ejs')
+});
+
+// new route post
+app.post('/pokemon', (req, res) => {
+	Pokemon.push(req.body)
+	res.redirect('/pokemon')
+});
 
 // index route
 app.get('/pokemon', (req, res) => {
@@ -26,9 +43,28 @@ app.get('/pokemon', (req, res) => {
 
 // show route
 app.get('/pokemon/:id', (req, res) => {
-
 	res.render('show.ejs', {
-		pokemon: Pokemon[req.params.id]
+		pokemon: Pokemon[req.params.id],
+		id: [req.params.id]
 	})
+});
+
+// edit route get
+app.get('/pokemon/:id/edit', (req, res) => {
+	res.render('edit.ejs', {
+		pokemon: Pokemon[req.params.id],
+		id: req.params.id
+	})
+});
+// edit route put
+app.put('/pokemon/:id', (req, res) => {
+	Pokemon[req.params.id] = req.body;
+	res.redirect('/pokemon/' + [req.params.id])
+});
+
+// DETEL route
+app.delete('/pokemon/:id', (req, res) => {
+	Pokemon.splice(req.params.id, 1)
+	res.redirect('/pokemon')
 });
 
